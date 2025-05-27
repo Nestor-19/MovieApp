89ac9396ae350e60ed97232743d56f27fc85cb75
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2025-05-26 20:47:42 -0400

    Refactor User model to use Lombok

--- backend/movieapp/src/main/java/com/example/movieapp/Models/User.java
+++ backend/movieapp/src/main/java/com/example/movieapp/Models/User.java
@@ -7,8 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.ArrayList;
+
 @Document(collection = "users")
 @Data
 @Builder
@@ -16,61 +16,12 @@ import java.util.ArrayList;
 @NoArgsConstructor
 public class User {
 
-
     @Id
     private String email;
     private String firstname;
     private String lastname;
     private int age;
-    private ArrayList<String> watchlist;
-    private ArrayList<String> watchedList;
+    private ArrayList<WatchListItem> watchlist;
     private UserRole role;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public ArrayList<String> getWatchlist() {
-        return watchlist;
-    }
-
-    public void setWatchlist(ArrayList<String> watchlist) {
-        this.watchlist = watchlist;
-    }
-
-    public ArrayList<String> getWatchedList() {
-        return watchedList;
-    }
-
-    public void setWatchedList(ArrayList<String> watchedList) {
-        this.watchedList = watchedList;
-    }
 }
