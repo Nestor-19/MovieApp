cca1b029451d1d87b4d06ae1a49d48b439acd35d
EvanNikaloasTheFirst <evangelosniko@outlook.com>
2025-05-27 21:34:06 +0100

    Dashboard with pagination complte

---
+++ frontend/src/components/Outbar.jsx
@@ -0,0 +1,14 @@
+export default function Outbar() {
+  return (
+        <header style={{ backgroundColor: '#365074' }}>
+        <nav className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+        {/* Logo */}
+        <div className="text-3xl font-bold text-white">MovieApp</div>
+
+        {/* Horizontal nav links */}
+
+
+      </nav>
+    </header>
+  );
+}
