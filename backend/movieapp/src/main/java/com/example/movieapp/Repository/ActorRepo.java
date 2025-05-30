f478847b17711745d5a107b2cc5b82b29b5597bb
EvanNikaloasTheFirst <evangelosniko@outlook.com>
2025-05-30 19:17:50 +0100

    Button to  push top 500 actors and top 100 movies

--- backend/movieapp/src/main/java/com/example/movieapp/Repository/ActorRepo.java
+++ backend/movieapp/src/main/java/com/example/movieapp/Repository/ActorRepo.java
@@ -1,2 +1,13 @@
-package com.example.movieapp.Repository;public interface ActorRepo {
+package com.example.movieapp.Repository;
+
+import com.example.movieapp.Models.Actor;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.ArrayList;
+
+public interface ActorRepo extends MongoRepository<Actor,String> {
+    Actor findByActorId(String id);
+
+    ArrayList<Actor> findAll();
+    Actor findByFullNameIgnoreCase(String fullname);
 }
--- backend/movieapp/src/main/java/com/example/movieapp/dtos/ActorDto.java
+++ backend/movieapp/src/main/java/com/example/movieapp/dtos/ActorDto.java
@@ -1,2 +1,41 @@
-package com.example.movieapp.dtos;public class ActorDto {
+package com.example.movieapp.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+
+@Getter
+@Setter
+public class ActorDto {
+
+
+    private String id;
+    private String name;
+    private String profile_path;
+
+    public String getProfile_path() {
+        return profile_path;
+    }
+
+    public void setProfile_path(String profile_path) {
+        this.profile_path = profile_path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
