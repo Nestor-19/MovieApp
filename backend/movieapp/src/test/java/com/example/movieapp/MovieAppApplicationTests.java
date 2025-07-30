baa2320429d5ca3786eef6abf7368e4c7ae9ebc9
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2025-07-30 19:58:08 -0400

    Override mongo uri in application.properties

--- backend/movieapp/src/test/java/com/example/movieapp/MovieAppApplicationTests.java
+++ backend/movieapp/src/test/java/com/example/movieapp/MovieAppApplicationTests.java
@@ -2,9 +2,23 @@ package com.example.movieapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
+@Testcontainers
 class MovieAppApplicationTests {
+
+	@Container
+  	static MongoDBContainer mongo = new MongoDBContainer("mongo:6.0.5");
+
+	@DynamicPropertySource
+	static void overrideMongoUri(DynamicPropertyRegistry registry) {
+    	registry.add("spring.data.mongodb.uri", mongo::getReplicaSetUrl);
+  	}
 
 	@Test
 	void contextLoads() {
